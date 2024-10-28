9f217303e4048b0b032da0eb7adab071648412a9
revan <revanescence33@gmail.com>
2024-10-28 11:11:49 +0530

    comminting

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,11 +16,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <NavBar/>
+      <SessionWrapper>
+      <NavBar/> 
       <div className="min-h-[84vh] bg-[#000000] [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       {children}
       </div>
       <Footer/>
+      </SessionWrapper>
       </body>
     </html>
   );
